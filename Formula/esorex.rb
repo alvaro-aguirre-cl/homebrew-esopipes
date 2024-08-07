75189227e4b9723132c30783c30fd58732a480e8
alvaro-aguirre-o <alvaro.aguirre.o@gmail.com>
2024-08-07 16:11:25 +0200

    test formula 2

--- Formula/esorex.rb
+++ Formula/esorex.rb
@@ -1,22 +1,21 @@
 class Esorex < Formula
-    desc "Execution Tool for European Southern Observatory pipelines"
-    homepage "https://www.eso.org/sci/software/cpl/"
-    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/esorex-3.13.8.tar.gz"
-    sha256 "5c024203d94331a08720bee9ea63e2ffb12bb6cf76005e4c69df782ea1b3d890"
-    license "GPL-2.0-or-later"
-  
-    livecheck do
-      url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/"
-      regex(/href=.*?esorex[._-]v?(\d+(?:\.\d+)+)\.t/i)
-    end
+  desc "Execution Tool for European Southern Observatory pipelines"
+  homepage "https://www.eso.org/sci/software/cpl/"
+  url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/esorex-3.13.8.tar.gz"
+  sha256 "5c024203d94331a08720bee9ea63e2ffb12bb6cf76005e4c69df782ea1b3d890"
+  license "GPL-2.0-or-later"
 
-  
-    depends_on "cpl"
-    depends_on "gsl"
-    depends_on "libffi"
-  
-    def install
-      system "./configure", "--disable-debug",
+  livecheck do
+    url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/"
+    regex(/href=.*?esorex[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "cpl"
+  depends_on "gsl"
+  depends_on "libffi"
+
+  def install
+    system "./configure", "--disable-debug",
                             "--disable-dependency-tracking",
                             "--disable-silent-rules",
                             "--prefix=#{prefix}",
@@ -24,11 +23,11 @@ class Esorex < Formula
                             "--with-gsl=#{Formula["gsl"].prefix}",
                             "--with-libffi=#{Formula["libffi"].prefix}",
                             "--with-included-ltdl"
-      system "make", "install"
-      inreplace prefix/"etc/esorex.rc", prefix/"lib/esopipes-plugins", HOMEBREW_PREFIX/"lib/esopipes-plugins"
-    end
-  
-    test do
-      assert_match "ESO Recipe Execution Tool, version #{version}", shell_output("#{bin}/esorex --version")
-    end
+    system "make", "install"
+    inreplace prefix/"etc/esorex.rc", prefix/"lib/esopipes-plugins", HOMEBREW_PREFIX/"lib/esopipes-plugins"
   end
+
+  test do
+    assert_match "ESO Recipe Execution Tool, version #{version}", shell_output("#{bin}/esorex --version")
+  end
+end
