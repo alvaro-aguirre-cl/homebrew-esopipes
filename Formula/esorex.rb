8da6a62bb3c3d8b7c1696cda7315930a7b2cc87e
alvaro-aguirre-o <alvaro.aguirre.o@gmail.com>
2024-08-14 01:27:08 +0200

    update all

--- Formula/esorex.rb
+++ Formula/esorex.rb
@@ -8,16 +8,11 @@ class Esorex < Formula
   url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/esorex-3.13.8.tar.gz"
   sha256 "5c024203d94331a08720bee9ea63e2ffb12bb6cf76005e4c69df782ea1b3d890"
   license "GPL-2.0-or-later"
-  revision 2
+  revision 3
 
   livecheck do
     url "https://ftp.eso.org/pub/dfs/pipelines/libraries/esorex/"
     regex(/href=.*?esorex[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  bottle do
-    root_url "https://github.com/alvaro-aguirre-cl/homebrew-esopipes/releases/download/esorex-3.13.8_2"
-    sha256 arm64_sonoma: "8775ea244ca69e5ed1a5ab0d02dda58f47c2c3a7cc935fc5360c72f8f55f56d4"
   end
 
   depends_on "cpl"
