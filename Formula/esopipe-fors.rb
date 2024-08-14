0fbacd09269df4eb46b3092c6c7cc24410201e72
alvaro-aguirre-cl <177130896+alvaro-aguirre-cl@users.noreply.github.com>
2024-08-14 13:42:52 +0000

    esopipe-fors: add 5.6.5-7_4 bottle.

--- Formula/esopipe-fors.rb
+++ Formula/esopipe-fors.rb
@@ -15,6 +15,14 @@ class EsopipeFors < Formula
     regex(/href=.*?fors-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/alvaro-aguirre-cl/homebrew-esopipes/releases/download/esopipe-fors-5.6.5-7_4"
+    sha256 arm64_sonoma: "72d2708ec99ca645fd982d393a490624d93962504df5887a6104e91815da60fd"
+    sha256 ventura:      "887b87b25548a49bd2df26986dff99c6ee26493c9b8ed214aed905f372c3e391"
+    sha256 monterey:     "cec11cb9dfb89de90e56202e6cdf6f3b743661a08fd50f534fbe8536b93f2f4c"
+    sha256 x86_64_linux: "478982d181b0999ca70e89a0f04a44e48cde6d9edd6965ee12f7fc1c6024fcaa"
+  end
+
   depends_on "cfitsio"
   depends_on "cpl"
   depends_on "curl"
