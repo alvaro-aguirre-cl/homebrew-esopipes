379e53537444db5c6caef4c6932e2f0c7068bd4e
alvaro-aguirre-cl <177130896+alvaro-aguirre-cl@users.noreply.github.com>
2024-08-14 13:42:52 +0000

    esopipe-crires: add 2.3.18-1_4 bottle.

--- Formula/esopipe-crires.rb
+++ Formula/esopipe-crires.rb
@@ -15,6 +15,14 @@ class EsopipeCrires < Formula
     regex(/href=.*?crire-kit-(\d+(?:[.-]\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/alvaro-aguirre-cl/homebrew-esopipes/releases/download/esopipe-crires-2.3.18-1_4"
+    sha256 cellar: :any,                 arm64_sonoma: "b915ab8e0fcef8732ea1928fab36919cca7638250f0bc82d6b635c0a1a17ef63"
+    sha256 cellar: :any,                 ventura:      "9ac7beb127c9938f623a91695e7b207258d474e62651d7973b8463fd54a81353"
+    sha256 cellar: :any,                 monterey:     "972d6993ca74c1f00881393b42a117977f30ec4abe94fe210becf4c4aeb9afd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "a5ed2a5f87f5ef0285da618ba32c7512ae9be832c73538d62df8e0d3bdbdddff"
+  end
+
   depends_on "cpl"
   depends_on "esorex"
   depends_on "libffi"
