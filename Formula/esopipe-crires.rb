a012827224721cd8fb611f43acc5a422c98227d4
alvaro-aguirre-cl <177130896+alvaro-aguirre-cl@users.noreply.github.com>
2024-10-03 11:11:57 +0000

    esopipe-crires: update 2.3.19 bottle.

--- Formula/esopipe-crires.rb
+++ Formula/esopipe-crires.rb
@@ -15,11 +15,8 @@ class EsopipeCrires < Formula
   end
 
   bottle do
-    root_url "https://github.com/alvaro-aguirre-cl/homebrew-esopipes/releases/download/esopipe-crires-2.3.18-1_4"
-    sha256 cellar: :any,                 arm64_sonoma: "b915ab8e0fcef8732ea1928fab36919cca7638250f0bc82d6b635c0a1a17ef63"
-    sha256 cellar: :any,                 ventura:      "9ac7beb127c9938f623a91695e7b207258d474e62651d7973b8463fd54a81353"
-    sha256 cellar: :any,                 monterey:     "972d6993ca74c1f00881393b42a117977f30ec4abe94fe210becf4c4aeb9afd7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a5ed2a5f87f5ef0285da618ba32c7512ae9be832c73538d62df8e0d3bdbdddff"
+    root_url "https://github.com/alvaro-aguirre-cl/homebrew-esopipes/releases/download/esopipe-crires-2.3.19"
+    sha256 cellar: :any, arm64_sonoma: "06f9d8aec18bcabb10c042a6d7442f7a3016190db937f14d65f1a82ad10d6e43"
   end
 
   depends_on "cpl"
