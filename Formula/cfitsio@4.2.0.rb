336ce121279ffef736a80be2e1afac9410a20262
alvaro-aguirre-cl <177130896+alvaro-aguirre-cl@users.noreply.github.com>
2024-10-15 00:56:04 +0000

    cfitsio@4.2.0 4.5.0

--- Formula/cfitsio@4.2.0.rb
+++ Formula/cfitsio@4.2.0.rb
@@ -1,8 +1,8 @@
 class CfitsioAT420 < Formula
   desc "C access to FITS data files with optional Fortran wrappers"
   homepage "https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html"
-  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.2.0.tar.gz"
-  sha256 "eba53d1b3f6e345632bb09a7b752ec7ced3d63ec5153a848380f3880c5d61889"
+  url "https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-4.5.0.tar.gz"
+  sha256 "e4854fc3365c1462e493aa586bfaa2f3d0bb8c20b75a524955db64c27427ce09"
   license "CFITSIO"
 
   livecheck do
